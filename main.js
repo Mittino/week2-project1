c1903520b4945d3675868e997f8756fd50e63c6f
Anna Mefford <annamefford@spencer-mefford-macbook-pro.local>
2016-07-20 21:47:22 -0600

    completed week1 project 2

--- main.js
+++ main.js
@@ -27,14 +27,30 @@ ADVANCED TRACK: 14-20
 */
 
 // 1. Create an object called "me" that describes you with your name, city, and job title.
+console.log('Question 1');
+
+var me = {
+  name:'Anna',
+  city:'Boulder',
+  jobTitle:'Region Administration Manager',
+};
+
+console.log(me);
 
 // 2. To your "me" object, add a key of "family" with the value being an array of three of your family members.
 // Example of desired output: { name: 'Bob Smith',
 //                              city: 'Denver',
 //                              jobTitle: 'professional wrestler',
 //                              family: ['Joan', 'Kyle', 'Hank'] }
+console.log('Qustion 2');
+me.family = (['Dan','Steve', 'Becca', 'Alli']);
+console.log(me);
+
 
 // 3. Using the "me" object, use dot notation and bracket notation to access the value of your city.
+console.log('Question 3');
+console.log(me.city);
+console.log(me['city']);
 
 // Use the following Object for questions 4-9:
 var invitees = { adults: [{
@@ -72,35 +88,85 @@ var invitees = { adults: [{
                       };
 
 // 4. Access Penny's dinner choice (this will be the second item in his dinnerChoice array)
+console.log('Question 4');
+console.log(invitees.adults[0].dinnerChoice[1]);
+
 
 // 5. Access Pablo's table number.
+console.log('Question 5');
+console.log(invitees.children[1].table);
 
 // 6. Access Lauren's seating specialConsideration.
+console.log('Question 6');
+console.log(invitees.adults[2].specialConsiderations.seating);
 
 // 7. Access Billy's dessert choice (this will be the third item in his dinnerChoice array)
+console.log('Question 7');
+console.log(invitees.adults[1].dinnerChoice[2]);
+
 
 // 8. Access Ada's allergies.
+console.log('Question 8');
+console.log(invitees.children[2].allergies);
 
 // 9. Create a string that reads Lauren's dinner choice in a sentence.
 // Desired result: "Lauren's appetizer is _______. She will be eating ______ for dinner
 // and has selected ________ as her dessert."
+console.log('Question 9');
+var dinnerString =('Lauren\'s appetizer is '+ invitees.adults[2].dinnerChoice[0] + '. She will be eating ' + invitees.adults[2].dinnerChoice[1] + ' for dinner and has selected ' + invitees.adults[2].dinnerChoice[2] + ' as her dessert.');
+console.log(dinnerString);
 
 // 10. Create an empty object called "jsWorkshop" using Constructor Notation.
+console.log('Question 10');
+var jsWorkshop = new Object ();
 
 // 11. To your "jsWorkshop" object, assign a key of "classmates" with the value being an array of five your classmates.
+console.log('Question 11');
+jsWorkshop.classmates = ['Kat','Richard','Thomas','Brian','Amanda'];
+console.log(jsWorkshop.classmates);
 
 // 12. Get the length of the fourth classmate's name.
+console.log('Qestion 12');
+console.log(jsWorkshop.classmates[3].length);
 
 // 13. Clear the classmates value.
-
+console.log('Question 13');
+jsWorkshop.classmates = [];
+console.log(jsWorkshop.classmates);
 
 // ADVANCED TRACK
 // 14. Create an empty object called "books".
+console.log('Question 14');
+var books = new Object ();
+console.log(books);
 
 // 15. Create three book objects that contain book titles, their author, and number of pages.
 //     The name of each object should be some kind of made up ISBN number.
 //     (Feel free to make up the titles. And the authors. Have fun. No pressure.)
 //     Insert the three objects into the "books" object.
+console.log('Question 15');
+
+var books = {
+  ISBN11111: {
+    title: 'Bob Loblaw\'s Law Blog',
+    author: 'Bob Lawblaw',
+    pages: 25
+  },
+
+ISBN22222: {
+  title: 'Hot Diggity',
+  author: 'Snoop Doggy Dog',
+  pages: 55
+  },
+
+ISBN33333: {
+  title: 'It\'s All Sunshine, Butterflies, and Rainbows',
+  author: 'Sunshine High',
+  pages: 55
+  },
+}
+
+console.log(books);
 
 // Final desired result:
     // var books = {ISBN22222:  {
@@ -121,12 +187,27 @@ var invitees = { adults: [{
     //             }
 
 // 16. Access the author of the first book.
+console.log('Question 16');
+console.log(books.ISBN11111.author);
 
 // 17. Get the length of the author the the third book.
+console.log('Question 17')
+console.log(books.ISBN33333.author.length);
 
 // 18. To each book object variable, add a key of "dateRead" with a value of the year you read the book.
 //     (Go ahead and make this up, too. Get wild.)
+console.log('Question 18');
+books.ISBN11111.dateRead = 'January 1, 2005'
+books.ISBN22222.dateRead = 'April 15, 2015'
+books.ISBN33333.dateRead = 'February 14, 2000'
+console.log(books.ISBN11111.dateRead);
+console.log(books.ISBN22222.dateRead);
+console.log(books.ISBN33333.dateRead);
 
 // 19. Remove the second book from the books object.
+console.log('Question 19');
+delete books.ISBN22222
+console.log(books);
 
 // 20. Explain, in your own words, the difference between Literal Notation and Constructor Notation.
+//In Literal Notation you literally write out each object and it's prperties. In Constructor Notation you create the object, and then add to it.
